fa9dbdab1ea9cbf704289231cafc6c121ab7ff47
Ruchir <rohanruchir@gmail.com>
2024-11-09 14:32:24 +0530

    Added support for Embed links and summary length option

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-
+import re
 import streamlit as st
 from dotenv import load_dotenv
 load_dotenv() #load all env variables
@@ -8,12 +8,12 @@ from youtube_transcript_api import YouTubeTranscriptApi
 import google.generativeai as genai
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
-prompt = """You are a Youtube video summarizer. You will be taking the transcript text and summarizing it and providing the important summary in points within 250 words. The transcript text will be appended after this"""
+prompt_template = os.getenv("SUMMARY_PROMPT")
 
 #fetch transcript data in the form of list and then append them to one single para.
 def extract_transcript_details(youtube_video_url):
     try:
-        video_id = youtube_video_url.split("=")[1]
+        video_id = extract_video_url(youtube_video_url)
         transcript_text = YouTubeTranscriptApi.get_transcript(video_id)
 
         transcript = ""
@@ -24,23 +24,44 @@ def extract_transcript_details(youtube_video_url):
 
     except Exception as e:
         raise e
+
+def extract_video_url(youtube_video_url):
+    """Extracts video ID from different types of YouTube URLs"""
+    pattern = r"(?:v=|\/|youtu\.be\/|\/embed\/|watch\?v=|watch\?.+&v=)([a-zA-Z0-9_-]{11})"
+    match = re.search(pattern, youtube_video_url)
+    if match:
+        return match.group(1)
+    else:
+        raise ValueError("Invalid YouTube URL")
+
+
 def generate_gemini_content(transcript_text, prompt):
     model = genai.GenerativeModel("gemini-pro")
     response = model.generate_content(prompt + transcript_text)
     return response.text
 
+#Streamlit UI
 st.title("YouTube Transcript Summarizer")
 yt_link = st.text_input("Enter YouTube URL")
 
 if yt_link:
-    video_id = yt_link.split("=")[1]
+    video_id = extract_video_url(yt_link)
     print(video_id)
     st.image(f"http://img.youtube.com/vi/{video_id}/0.jpg", use_container_width=True)
 
+summary_length = st.radio("Choose Summary Length", ["Short", "Medium", "Long"])
+
+if summary_length == "Short":
+    prompt = prompt_template.replace("{summary_length}", "short (3 points)")
+elif summary_length == "Medium":
+    prompt = prompt_template.replace("{summary_length}", "medium (3-5 points, max 5 points)")
+else:
+    prompt = prompt_template.replace("{summary_length}", "long (5-8 points, max 8 points)")
+# start summarizing
 if st.button("Get Summary"):
     transcript_text = extract_transcript_details(yt_link)
 
     if transcript_text:
         summary = generate_gemini_content(transcript_text, prompt)
-        #st.markdown("Summary:")
+        st.markdown("## Summary:")
         st.write(summary)
